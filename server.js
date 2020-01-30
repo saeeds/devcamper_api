ed5c10d732d73a737f184ca0dc6cb3ed7b11f678
Saeed Alsharqawi <saed_sharqawi@hotmail.com>
2020-01-31 02:00:16 +0300

    create middleware, controller, utils

--- server.js
+++ server.js
@@ -1,13 +1,42 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const errorHandler = require('./middleware/error');
+const connectDB = require('./config/db');
 
-//Load en vars
-dotenv.config({ path: './config/config.env' })
+//Load env variables
+dotenv.config({ path: './config/config.env' });
 
+//Connect to database
+connectDB();
+
+//Route Files
+const bootcamps = require('./routes/bootcamps');
 const app = express();
 
-const PORT = process.env.PORT;
+//Body parser 
+app.use(express.json());
+//Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('combined'));
+}
 
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}!`);
+//Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+app.use(errorHandler);
+
+//get the prot form config file or 5000 as default
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}!`.yellow.bold);
+});
+
+//Handle unhandled promise rejictions
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  //close server & exit process
+  server.close(() => process.exit(1));
 });
